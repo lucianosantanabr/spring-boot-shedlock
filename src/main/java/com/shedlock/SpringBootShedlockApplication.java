f5d8ca62a92fa619cade50a629077fbeb7a7d4c3
Luciano Santana <lucianosantanabr@gmail.com>
2021-07-31 17:15:39 -0300

    scheduleLock projeto para varias instancias

---
+++ src/main/java/com/shedlock/SpringBootShedlockApplication.java
@@ -0,0 +1,13 @@
+package com.shedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShedlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShedlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shedlock/SpringBootShedlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShedlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
