8b6c7b25e00d4ebeebeb27a7bd746978d8a226eb
Luciano S. Santana <lucianosantanabr@gmail.com>
2024-07-03 15:05:54 -0300

    Update JobReport.java

--- src/main/java/com/shedlock/JobReport.java
+++ src/main/java/com/shedlock/JobReport.java
@@ -1,1 +1,22 @@
-package com.shedlock;import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;import org.springframework.scheduling.annotation.Scheduled;import org.springframework.stereotype.Component;@Componentpublic class JobReport {  @Scheduled(cron = "0 0 12 * * *")  @SchedulerLock(name = "jobReport", lockAtMostFor = "30m")  public void report() {    // report revenue based on e.g. orders in database    System.out.println("Reporting revenue");  }  @Scheduled(cron = "0 * * * * *")  @SchedulerLock(name = "shortRunningTask", lockAtMostFor = "50s", lockAtLeastFor = "30s")  public void shortRunningTask() {    System.out.println("Start short running task");  }}
+package com.shedlock;
+
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobReport {
+
+  @Scheduled(cron = "0 0 12 * * *")
+  @SchedulerLock(name = "jobReport", lockAtMostFor = "30m")
+  public void report() {
+    // report revenue based on e.g. orders in database
+    System.out.println("Reporting revenue");
+  }
+
+  @Scheduled(cron = "0 * * * * *")
+  @SchedulerLock(name = "shortRunningTask", lockAtMostFor = "50s", lockAtLeastFor = "30s")
+  public void shortRunningTask() {
+    System.out.println("Start short running task");
+  }
+}
